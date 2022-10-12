832b403a05744ad766f2822f76e663dcaabca5eb
Gui-dev <adrianon10@gmail.com>
2022-10-12 18:25:11 -0300

    chore: add eslint config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-   './src/**/*.tsx'
+    './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
