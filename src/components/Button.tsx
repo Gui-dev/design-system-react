0c011033c710a3dd025886bd2ed3c4e399ced97e
Gui-dev <adrianon10@gmail.com>
2022-10-16 19:59:58 -0300

    feat: add ButtonProps

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,24 @@
 import { Slot } from '@radix-ui/react-slot'
 import { clsx } from 'clsx'
+import { ButtonHTMLAttributes } from 'react'
 
-export type ButtonProps = {
-  title: string
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: string
   asChild?: boolean
+  className?: string
 }
 
-export const Button = ({ title, asChild }: ButtonProps) => {
+export const Button = ({ children, asChild, className, ...rest }: ButtonProps) => {
   const Component = asChild ? Slot : 'button'
   return (
     <Component
       className={clsx(
-        'text-sm font-semibold text-black py-4 px-3 w-full bg-cyan-500 rounded transition-colors hover:bg-cyan-300 focus:ring-2 ring-white'
+        'text-sm font-semibold text-black py-3 px-4 w-full bg-cyan-500 rounded transition-colors hover:bg-cyan-300 focus:ring-2 ring-white',
+        className
       )}
+      {...rest}
     >
-      {title}
+      {children}
     </Component>
   )
 }
