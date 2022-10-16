8e2fc3e85e8b21581720a88b560d6910005969a6
Gui-dev <adrianon10@gmail.com>
2022-10-16 19:59:30 -0300

    feat: add CheckboxProps

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,10 +1,13 @@
 import * as RadixCheckbox from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
 
-export const Checkbox = () => {
+export interface CheckboxProps extends RadixCheckbox.CheckboxProps { }
+
+export const Checkbox = (props: CheckboxProps) => {
   return (
     <RadixCheckbox.Root
       className="p-[2px] h-6 w-6 bg-gray-800 rounded"
+      {...props}
     >
       <RadixCheckbox.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500" />
